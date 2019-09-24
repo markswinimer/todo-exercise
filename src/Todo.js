f5c359dda183407e6d4007775efe7d06d77c3892
markswinimer <markswinimer@gmail.com>
2019-09-23 22:15:07 -0400

    initial commit, remove cra items

---
+++ src/Todo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import "./Todo.css";
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // STATE
+    }
+    // BIND
+  }
+  render() {
+    return(
+      <div className="Todo">
+        <h1>Todo List</h1>
+      </div>
+    )
+  }
+
+}
+export default Todo;
